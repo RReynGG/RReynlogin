1ac1960063d7fcb7cafe0776c7f9ff32f81d4d56
RReynGG <kazimzadxaliddin1@gmail.com>
2026-03-28 12:20:48 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-        const signUpButton=document.getElementById('signUpButton');
-        const signInButton=document.getElementById('signInButton');
-        const signInForm=document.getElementById('signIn');
-        const singUpForm=document.getElementById('signUp');
+        // Formlar arası keçid
+        const signUpButton = document.getElementById('signUpButton');
+        const signInButton = document.getElementById('signInButton');
+        const signInForm = document.getElementById('signIn');
+        const signUpForm = document.getElementById('signUp');
 
-        signUpButton.addEventListener('click', function(){
-            signInForm.style.display="none";
-            singUpForm.style.display="block";
-        })
-        
-        signInButton.addEventListener('click', function(){
-            signInForm.style.display="block";
-            singUpForm.style.display="none";
-        })
+        signUpButton.addEventListener('click', () => {
+            signInForm.style.display = "none";
+            signUpForm.style.display = "block";
+        });
 
-        
+        signInButton.addEventListener('click', () => {
+            signInForm.style.display = "block";
+            signUpForm.style.display = "none";
+        });
+
+        // Email Validasiyası
+        const regForm = document.getElementById('regForm');
+        regForm.addEventListener('submit', function(event) {
+            const emailInput = document.getElementById('email');
+            if (!emailInput.value.endsWith('@gmail.com')) {
+                event.preventDefault();
+                alert("Xəta: Yalnız @gmail.com ünvanları qəbul edilir!");
+                emailInput.style.borderColor = "var(--neon-pink)";
+            }
+        });
